5eaabefa94371b9666926c2b7e851ad7edacae8c
sokolw <sokolw@users.noreply.github.com>
2020-09-06 20:21:29 +0300

    перебор чисел 1-5

--- myhomework/Program.cs
+++ myhomework/Program.cs
@@ -7,6 +7,10 @@ namespace myhomework
         static void Main(string[] args)
         {
             Random random = new Random();
+            int[] arr = new int[5] {1,2,3,4,5};
+            Console.WriteLine("Numbers between 1-5");
+            foreach(int e in arr)
+                Console.WriteLine(arr);
             Console.WriteLine("Hello World! You random number it" + random.Next(1-3));
         }
     }
