64ce4173286826a4e176019e424805b7648c8222
sokolw <sokolw@users.noreply.github.com>
2020-09-06 20:14:18 +0300

    add new project
    test

---
+++ myhomework/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace myhomework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
