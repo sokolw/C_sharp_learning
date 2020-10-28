93ee55f24699c1cc0401645b055ec69d086617b8
sokolw <sokolw@users.noreply.github.com>
2020-10-28 13:52:03 +0300

    Copy

---
+++ homework/Millionaires/Question.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Millionaires
+{
+    class Question
+    {
+        public string QuestionText { get; private set; }
+        public Answer[] Answers { get; private set; }
+
+        public Question(string questionText, Answer[] answers)
+        {
+            QuestionText = questionText;
+            Answers = answers;
+        }
+    }
+}
